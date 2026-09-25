07461334d66d5ee63b9ee6358009d063774b8f32
Shashank Kumar dev <shashankkumardev@gmail.com>
2026-09-25 18:15:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (99.74%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-       set<int> st;
-       for(int x:nums){
-        st.insert(x);
-         if(st.size()>3){
-        st.erase(st.begin());
-       }
-       }
-      
-       if(st.size()<3){
-        return *st.rbegin();
-       }
-       return *st.begin();
-       
+       long long first = LLONG_MIN;
+        long long second = LLONG_MIN;
+        long long third = LLONG_MIN;
+
+        for(int x : nums) {
+
+            // Ignore duplicate
+            if(x == first || x == second || x == third) {
+                continue;
+            }
+
+            if(x > first) {
+                third = second;
+                second = first;
+                first = x;
+            }
+            else if(x > second) {
+                third = second;
+                second = x;
+            }
+            else if(x > third) {
+                third = x;
+            }
+        }
+
+        // Third maximum doesn't exist
+        if(third == LLONG_MIN) {
+            return first;
+        }
+
+        return third;
     }
 };
